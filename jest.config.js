b11bed25b2b3ac54b35c81f37dea6be1a297cd34
xobotyi <xog3@yandex.ru>
2021-03-12 14:01:17 +0300

    chore: dependabot configuration along with GH workflows.

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   coverageDirectory: './coverage',
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: ['src/**/*.ts'],
   globals: {
     'ts-jest': {
